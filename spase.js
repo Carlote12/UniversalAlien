54655f277a08fbfbe2e5e02bd974c97745e4ad6f
Carlote12 <carloaugustocatalini@gmail.com>
2024-12-27 16:04:48 -0300

    nuevo juego

---
+++ spase.js
@@ -0,0 +1,16 @@
+
+let tileSize = 32;
+let rows = 16;
+let columns = 16;
+
+let boards;
+let boardwidth = titleSize * columns; //32*16
+let boardHigth = titleSize * rows;
+let context;
+
+let board
+
+window.onload = function() {
+    board = document.getElementById('board');
+    
+}
